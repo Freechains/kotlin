17b40cab62b5edca7df4807fa70acc93093b56e5
Francisco Sant'Anna <francisco.santanna@gmail.com>
2020-02-11 16:16:42 -0200

    (>) more tests

--- src/main/make/Tests.sh
+++ src/main/make/Tests.sh
@@ -13,7 +13,26 @@ freechains --host=localhost:8400 chain get /0 0_6e340b9cffb37a989ca544e6bb780a2c
 
 set -e
 diff /tmp/freechains-tests-get-1.out  tests/freechains-tests-get-1.out
+set +e
 
+rm -Rf /tmp/8401
+freechains host create /tmp/8401 8401
+freechains host stop --host=localhost:8401
+freechains host start /tmp/8401 &
+sleep 0.5
+freechains --host=localhost:8401 chain create /0
+freechains --host=localhost:8400 chain put /0 text 111
+freechains --host=localhost:8400 chain put /0 text 222
+freechains --host=localhost:8400 chain send /0 localhost:8401
+sleep 0.5
+
+set -e
+diff /tmp/8400/chains/0/ /tmp/8401/chains/0/
+ret=`ls /tmp/8400/chains/0/ | wc`
+if [ "$ret" != "      4       4     288" ]; then
+  echo "$ret"
+  exit 1
+fi
 set +e
 
 echo
