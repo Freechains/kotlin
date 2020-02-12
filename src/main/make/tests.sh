6b019a2568c9f7f631f07882bced70a3170ea6fc
fsantanna <francisco.santanna@gmail.com>
2020-02-12 14:28:12 -0300

    (>) tests

--- src/main/make/tests.sh
+++ src/main/make/tests.sh
@@ -13,13 +13,19 @@ freechains host stop --host=localhost:8400
 freechains host start /tmp/freechains/8400 &
 sleep 0.5
 freechains --host=localhost:8400 chain create /0
+g=`freechains --host=localhost:8400 chain genesis /0`
 h=`freechains --host=localhost:8400 chain put /0 text Hello_World`
 freechains --host=localhost:8400 chain get /0 "$h" > /tmp/freechains/freechains-tests-get-1.out
 freechains --host=localhost:8400 chain get /0 0_6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d > /tmp/freechains/freechains-tests-get-0.out
+hs=`freechains --host=localhost:8400 chain heads /0`
+freechains --host=localhost:8400 chain get /0 "$g" > /tmp/freechains/freechains-tests-gen.out
+freechains --host=localhost:8400 chain get /0 "$hs" > /tmp/freechains/freechains-tests-heads.out
 
 set -e
+diff /tmp/freechains/freechains-tests-gen.out   tests/freechains-tests-get-0.out
 diff /tmp/freechains/freechains-tests-get-0.out tests/freechains-tests-get-0.out
 diff /tmp/freechains/freechains-tests-get-1.out tests/freechains-tests-get-1.out
+diff /tmp/freechains/freechains-tests-heads.out tests/freechains-tests-get-1.out
 set +e
 
 ###############################################################################
